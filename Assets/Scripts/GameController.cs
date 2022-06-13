4df3e168aaa88bd42fd6cf82a8892a8c557fb7e1
Danil Goncharov <bigwitch3r@gmail.com>
2022-06-13 19:35:27 +0300

    Добавлено динамическое изменение среды
    Динамическая смена цвета заднего фона, а также движения камеры

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -10,11 +10,16 @@ public class GameController : MonoBehaviour
     public float cubeChangePlaceSpeed = 0.5f;
     public GameObject cubeToCreate, allCubes;
     public GameObject[] canvasStartPage;
+    public Color[] bgColors;
 
     private CubePos nowCube = new CubePos(0, 1, 0);
     private Rigidbody allCubesRb;
     private bool IsLose, firstCube;
     private Coroutine showCubePlace;
+    private float camMoveToYPosition, camMoveSpeed = 2f;
+    private Transform mainCam;
+    private int prevCountMaxHorizontal;
+    private Color toCameraColor;
 
     private List<Vector3> allCubesPositions = new List<Vector3>
     {
@@ -32,13 +37,17 @@ public class GameController : MonoBehaviour
 
     private void Start()
     {
+        toCameraColor = Camera.main.backgroundColor;
+        mainCam = Camera.main.transform;
+        camMoveToYPosition = 5.9f + nowCube.y - 1f;
+
         allCubesRb = allCubes.GetComponent<Rigidbody>();
         showCubePlace = StartCoroutine(ShowCubePlace());
     }
 
     private void Update()
     {
-        if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace != null)
+        if ((Input.GetMouseButtonDown(0) || Input.touchCount > 0) && cubeToPlace != null && allCubes != null && !EventSystem.current.IsPointerOverGameObject())
         {
 
 #if !UNITY_EDITOR
@@ -47,11 +56,6 @@ public class GameController : MonoBehaviour
                 return;
             }
 #endif
-
-            if (EventSystem.current.IsPointerOverGameObject())
-            {
-                return;
-            }
 
             if (!firstCube)
             {
@@ -73,6 +77,7 @@ public class GameController : MonoBehaviour
             allCubesRb.isKinematic = false;
 
             SpawnPositions();
+            MoveCameraChangeBg();
         }
 
         if (!IsLose && allCubesRb.velocity.magnitude > 0.1f)
@@ -82,6 +87,14 @@ public class GameController : MonoBehaviour
             StopCoroutine(showCubePlace);
         }
 
+        mainCam.localPosition = Vector3.MoveTowards(mainCam.localPosition,
+            new Vector3(mainCam.localPosition.x, camMoveToYPosition, mainCam.localPosition.z),
+            camMoveSpeed * Time.deltaTime);
+
+        if (Camera.main.backgroundColor != toCameraColor)
+        {
+            Camera.main.backgroundColor = Color.Lerp(Camera.main.backgroundColor, toCameraColor, Time.deltaTime / 1.5f);
+        }
     }
 
     IEnumerator ShowCubePlace()
@@ -135,7 +148,18 @@ public class GameController : MonoBehaviour
             positions.Add(new Vector3(nowCube.x, nowCube.y, nowCube.z - 1));
         }
 
-        cubeToPlace.position = positions[UnityEngine.Random.Range(0, positions.Count)];
+        if (positions.Count > 1)
+        {
+            cubeToPlace.position = positions[UnityEngine.Random.Range(0, positions.Count)];
+        }
+        else if (positions.Count == 0)
+        {
+            IsLose = true;
+        }
+        else
+        {
+            cubeToPlace.position = positions[0];
+        }
     }
 
     private bool isPositionEmpty(Vector3 targetPos)
@@ -154,6 +178,52 @@ public class GameController : MonoBehaviour
         }
 
         return true;
+    }
+
+    private void MoveCameraChangeBg()
+    {
+        int maxX = 0, maxY = 0, maxZ = 0, maxHor;
+
+        foreach (Vector3 pos in allCubesPositions)
+        {
+            if (Mathf.Abs(Convert.ToInt32(pos.x)) > maxX)
+            {
+                maxX = Mathf.Abs(Convert.ToInt32(pos.x));
+            }
+
+            if (Convert.ToInt32(pos.y) > maxY)
+            {
+                maxY = Convert.ToInt32(pos.y);
+            }
+
+            if (Mathf.Abs(Convert.ToInt32(pos.z)) > maxZ)
+            {
+                maxZ = Mathf.Abs(Convert.ToInt32(pos.z));
+            }
+        }
+
+        camMoveToYPosition = 5.9f + nowCube.y - 1f;
+
+        maxHor = maxX > maxZ ? maxX : maxZ;
+
+        if (maxHor % 3 == 0 && prevCountMaxHorizontal != maxHor)
+        {
+            mainCam.localPosition -= new Vector3(0, 0, 2.5f);
+            prevCountMaxHorizontal = maxHor;
+        }
+
+        if (maxY >= 7)
+        {
+            toCameraColor = bgColors[2];
+        }
+        else if (maxY >= 5)
+        {
+            toCameraColor = bgColors[1];
+        }
+        else if (maxY >= 2)
+        {
+            toCameraColor = bgColors[0];
+        }
     }
 }
 
